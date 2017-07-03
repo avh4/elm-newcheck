7b512ca691ac2b34b65030d50a240894b9184639
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-07-02 21:09:36 -0700

    Show action log when tests fail

--- src/Fuzz/Action.elm
+++ src/Fuzz/Action.elm
@@ -14,30 +14,68 @@ type alias Action real test =
     }
 
 
+type alias StepValue real test =
+    ( real, test, Log test )
+
+
+{-| A log of the state transitions that occurred during a test run.
+Use for display if the test fails.
+-}
+type alias Log test =
+    { init : test
+    , steps : List ( String, test )
+    }
+
+
 run :
     Action real test
-    -> Result String ( real, test )
-    -> Result String ( real, test )
+    -> Result ( String, String, Log test ) (StepValue real test)
+    -> Result ( String, String, Log test ) (StepValue real test)
 run action previousResult =
     case previousResult of
         Err _ ->
             previousResult
 
-        Ok ( real, test ) ->
+        Ok ( real, test, log ) ->
             -- TODO: check precondition
             case action.go real test of
                 ( _, _, Err reason ) ->
-                    Err ("Post condition failed: " ++ action.name ++ "\n" ++ reason)
+                    Err ( action.name, reason, log )
 
                 ( newReal, newTest, Ok () ) ->
-                    Ok ( newReal, newTest )
+                    Ok
+                        ( newReal
+                        , newTest
+                        , { log | steps = ( action.name, newTest ) :: log.steps }
+                        )
 
 
 test : List (Action real test) -> real -> test -> Expectation
 test actions initialReal initialTestModel =
     List.foldl
         run
-        (Ok ( initialReal, initialTestModel ))
+        (Ok ( initialReal, initialTestModel, Log initialTestModel [] ))
         actions
-        |> Result.map (always ())
-        |> Expect.equal (Ok ())
+        |> showResult
+
+
+showResult : Result ( String, String, Log test ) (StepValue real test) -> Expectation
+showResult result =
+    let
+        showStep ( step, output ) =
+            step ++ "  -->  " ++ toString output
+    in
+    case result of
+        Err ( last, message, log ) ->
+            Expect.fail
+                (showStep ( "<init>", log.init )
+                    ++ "\n"
+                    ++ String.join "\n" (List.map showStep log.steps)
+                    ++ "\n"
+                    ++ last
+                    ++ "  ==>  FAILED\n\n    "
+                    ++ message
+                )
+
+        Ok _ ->
+            Expect.pass
