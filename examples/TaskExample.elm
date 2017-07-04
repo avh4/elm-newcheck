1c5589cef944822c24aa2d83b9d88e6eec58c86f
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-07-04 13:20:49 -0700

    WIP UI with style-elements

--- examples/TaskExample.elm
+++ examples/TaskExample.elm
@@ -1,12 +1,16 @@
 module TaskExample exposing (main)
 
 import CircularBuffer
+import Color
+import Element exposing (Element)
+import Element.Attributes exposing (..)
 import Fuzz
 import Fuzz.Action.Log exposing (Log)
 import Fuzz.Action.Task exposing (..)
 import Html exposing (..)
-import Html.Attributes exposing (style)
 import Random.Pcg
+import Style
+import Style.Color exposing (..)
 import Task
 
 
@@ -95,30 +99,80 @@ main =
         , update = update
         , subscriptions = \model -> Sub.none
         , view =
-            \model ->
-                model.runs
-                    -- |> List.filter (\log -> log.failure /= Nothing)
-                    |> List.reverse
-                    |> List.map viewRun
-                    |> Html.div []
+            Element.layout defaultStyles << view
         }
 
 
-viewRun : Log test -> Html msg
+view : Model test -> Element Class Variation msg
+view model =
+    model.runs
+        -- |> List.filter (\log -> log.failure /= Nothing)
+        |> List.reverse
+        |> List.map viewRun
+        |> Element.column TestRuns []
+
+
+type Class
+    = TestRuns
+    | TestRun
+    | Log
+    | LogInit
+    | LogSteps
+    | LogStep
+    | LogStepName
+    | LogStepModel
+
+
+type Variation
+    = Failed
+    | Passed
+
+
+viewRun : Log test -> Element Class Variation msg
 viewRun log =
-    div
-        [ style
-            [ ( "color"
-              , if log.failure == Nothing then
-                    "lightgray"
-                else
-                    "auto"
-              )
-            ]
+    Element.el TestRun
+        [ vary Failed (log.failure /= Nothing)
+        , vary Passed (log.failure == Nothing)
+        , padding 8
         ]
-        [ Html.text <| toString log ]
+        (viewLog log)
+
+
+viewLog : Log test -> Element Class Variation msg
+viewLog log =
+    let
+        viewStep ( name, testModel ) =
+            Element.row LogStep
+                []
+                [ Element.el LogStepName [ width <| fill 1 ] (Element.text name)
+                , Element.el LogStepModel [ width <| fill 1 ] (Element.text <| toString testModel)
+                ]
+    in
+    Element.column Log
+        []
+        [ Element.el LogInit [] (Element.text <| toString log.init)
+        , Element.column LogSteps [] (List.map viewStep log.steps)
+        ]
 
 
 (>>>) : (b -> c -> d) -> (d -> x) -> (b -> c -> x)
 (>>>) base f b c =
     f (base b c)
+
+
+defaultStyles : Style.StyleSheet Class Variation
+defaultStyles =
+    Style.styleSheet
+        [ Style.style TestRun
+            [ background Color.lightGrey
+            , Style.Color.text Color.charcoal
+            , Style.variation Passed
+                [ background Color.lightGreen
+                , Style.Color.text Color.darkGreen
+                ]
+            , Style.variation Failed
+                [ background Color.lightRed
+                , Style.Color.text Color.darkRed
+                ]
+            ]
+        ]
